d12d621dd1f0516b3f2b5eb4c867775a5b11cffb
Cheranga Hatangala <cheranga@gmail.com>
2019-04-15 07:01:46 +1000

    Adding a "WarmUp" function which is like a dummy function, so that the other important functions will have a reduced cold start.

---
+++ FunkyBank.CustomersApi/Functions/WarmupFunction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace FunkyBank.CustomersApi.Functions
+{
+    public static class WarmUpFunction
+    {
+        [FunctionName("Warmup")]
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "warmup")]
+            HttpRequest request,
+            ILogger logger)
+        {
+            logger.LogInformation("Warmup function got called");
+
+            return new OkResult();
+        }
+    }
+}
